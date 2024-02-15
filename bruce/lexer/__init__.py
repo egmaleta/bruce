d42955b1efffeee8c35e7b4e6b5f406f83e75e5f
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-02-15 10:26:06 -0500

    add minimal lexer impl

---
+++ bruce/lexer/__init__.py
@@ -0,0 +1,50 @@
+from re import Pattern
+from dataclasses import dataclass
+
+
+@dataclass
+class Token:
+    lexeme: str
+    type: str
+
+
+def _match(prefix: Pattern | str, code: str):
+    if isinstance(prefix, str):
+        return prefix if code.startswith(prefix) else None
+
+    match = prefix.match(code)
+    return match.string if match != None else None
+
+
+def create_lexer(
+    prefixes: list[tuple[Pattern | str, str]],
+    ignored_prefixes: list[Pattern | str],
+):
+    all_prefixes: list[tuple[Pattern | str, str | None]] = prefixes + [
+        (pattern, None) for pattern in ignored_prefixes
+    ]
+
+    def lexer(code: str):
+        tokens = []
+
+        i = 0
+        while i < len(code):
+            rest_of_code = code[i:]
+
+            for prefix, type in all_prefixes:
+                lexeme = _match(prefix, rest_of_code)
+                if lexeme != None:
+                    i += len(lexeme)
+                    if type != None:
+                        tokens.append(Token(lexeme, type))
+                    break
+            else:
+                # raise error
+                pass
+
+        return tokens
+
+    return lexer
+
+
+defualt_lexer = create_lexer([], [])
