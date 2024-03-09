f47fda40b7fd383aa8e8556f0e16d321a5166692
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-03-09 09:40:21 -0500

    support optional semicolons inside block expression fix Comparison production

--- bruce/__init__.py
+++ bruce/__init__.py
@@ -34,51 +34,48 @@ number, string, identifier = GRAMMAR.add_terminals("number string id")
 # endregion
 
 # region NON TERMINALS
+
+TypeAnnotation, OptionalSemicolon = GRAMMAR.add_non_terminals(
+    "type_annotation opt_semicolon"
+)
+Params, MoreParams = GRAMMAR.add_non_terminals("params more_params")
+Args, MoreArgs = GRAMMAR.add_non_terminals("args more_args")
+
 Program = GRAMMAR.add_non_terminal("program", True)
 TypeDecl, TypeDeclList = GRAMMAR.add_non_terminals("type_decl type_decls")
 MemberDecl, MemberDeclList, Member = GRAMMAR.add_non_terminals(
     "member_decl member_decls member"
 )
-Params, MoreParams = GRAMMAR.add_non_terminals("params more_params")
 MethodBody = GRAMMAR.add_non_terminal("method_body")
-TypeAnnotation = GRAMMAR.add_non_terminal("type_annotation")
+
 Expr = GRAMMAR.add_non_terminal("expr")
-LetExpr, Binding, MoreBindings = GRAMMAR.add_non_terminals(
-    "let_expr binding more_bindings"
-)
-BranchExpr, ElseBlock = GRAMMAR.add_non_terminals("if_expr else_block")
-LoopExpr = GRAMMAR.add_non_terminal("loop_expr")
-BlockExpr, MoreExprs = GRAMMAR.add_non_terminals("block_expr more_exprs")
+Binding, MoreBindings = GRAMMAR.add_non_terminals("binding more_bindings")
+ElseBranch, ElseStmtBranch = GRAMMAR.add_non_terminals("else_branch else_stmt_branch")
+BlockExpr, Stmt, MoreStmts = GRAMMAR.add_non_terminals("block_expr stmt more_stmts")
+
 Disj, MoreDisjs = GRAMMAR.add_non_terminals("disj more_disjs")
 Conj, MoreConjs = GRAMMAR.add_non_terminals("conj more_conjs")
-Comparison, Comparer = GRAMMAR.add_non_terminals("comparison comparer")
+Comparison = GRAMMAR.add_non_terminal("comparison")
 Arith = GRAMMAR.add_non_terminal("arith")
 Term, MoreTerms = GRAMMAR.add_non_terminals("term more_terms")
 Factor, MoreFactors = GRAMMAR.add_non_terminals("factor more_factors")
 Base, Powers = GRAMMAR.add_non_terminals("base powers")
-Args, MoreArgs = GRAMMAR.add_non_terminals("args more_args")
 Atom, Action, Mutation = GRAMMAR.add_non_terminals("atom action mutation")
 
 # endregion
 
 # region PRODUCTIONS
 
-Program %= TypeDeclList + Expr, None, None, None
-
-TypeDeclList %= TypeDecl + TypeDeclList, None, None, None
-TypeDeclList %= GRAMMAR.Epsilon, None
-
-TypeDecl %= type_k + lbrace + MemberDeclList + rbrace, None, None, None, None, None
-
-MemberDeclList %= MemberDecl + semicolon + MemberDeclList, None, None, None, None
-MemberDeclList %= GRAMMAR.Epsilon, None
-
-MemberDecl %= identifier + Member, None, None, None
-Member %= bind + TypeAnnotation + Expr, None, None, None, None
-Member %= lparen + Params + rparen + MethodBody, None, None, None, None, None
-
 TypeAnnotation %= colon + identifier, None, None, None
 TypeAnnotation %= GRAMMAR.Epsilon, None
+
+OptionalSemicolon %= semicolon, None, None
+OptionalSemicolon %= GRAMMAR.Epsilon, None
+
+Args %= Expr + MoreArgs, None, None, None
+Args %= GRAMMAR.Epsilon, None
+MoreArgs %= comma + Expr + MoreArgs, None, None, None, None
+MoreArgs %= GRAMMAR.Epsilon, None
 
 Params %= identifier + TypeAnnotation + MoreParams, None, None, None, None
 Params %= GRAMMAR.Epsilon, None
@@ -92,30 +89,9 @@ MoreParams %= (
 )
 MoreParams %= GRAMMAR.Epsilon, None
 
-MethodBody %= BlockExpr, None, None
-MethodBody %= then + Expr, None, None, None
-
-Expr %= LetExpr, None, None
-Expr %= BranchExpr, None, None
-Expr %= LoopExpr, None, None
-Expr %= BlockExpr, None, None
-Expr %= Disj + MoreDisjs, None, None, None
-
-LetExpr %= (
-    let + Binding + MoreBindings + in_k + Expr,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-)
-Binding %= identifier + TypeAnnotation + bind + Expr, None, None, None, None, None
-MoreBindings %= comma + Binding + MoreBindings, None, None, None, None
-MoreBindings %= GRAMMAR.Epsilon, None
-
-BranchExpr %= (
-    if_k + lparen + Expr + rparen + Expr + ElseBlock,
+Expr %= let + Binding + MoreBindings + in_k + Expr, None, None, None, None, None, None
+Expr %= (
+    if_k + lparen + Expr + rparen + Expr + ElseBranch,
     None,
     None,
     None,
@@ -124,20 +100,8 @@ BranchExpr %= (
     None,
     None,
 )
-ElseBlock %= (
-    elif_k + lparen + Expr + rparen + Expr + ElseBlock,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-)
-ElseBlock %= else_k + Expr, None, None, None
-
-LoopExpr %= while_k + lparen + Expr + rparen + Expr, None, None, None, None, None, None
-LoopExpr %= (
+Expr %= while_k + lparen + Expr + rparen + Expr, None, None, None, None, None, None
+Expr %= (
     for_k + lparen + identifier + in_k + Expr + rparen + Expr,
     None,
     None,
@@ -148,10 +112,69 @@ LoopExpr %= (
     None,
     None,
 )
+Expr %= BlockExpr, None, None
+Expr %= Disj + MoreDisjs, None, None, None
 
-BlockExpr %= lbrace + Expr + MoreExprs + rbrace, None, None, None, None, None
-MoreExprs %= semicolon + Expr + MoreExprs, None, None, None, None
-MoreExprs %= GRAMMAR.Epsilon, None
+Binding %= identifier + TypeAnnotation + bind + Expr, None, None, None, None, None
+MoreBindings %= comma + Binding + MoreBindings, None, None, None, None
+MoreBindings %= GRAMMAR.Epsilon, None
+
+ElseBranch %= (
+    elif_k + lparen + Expr + rparen + Expr + ElseBranch,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+)
+ElseBranch %= else_k + Expr, None, None, None
+
+BlockExpr %= lbrace + Stmt + MoreStmts + rbrace, None, None, None, None, None
+
+# statements are the same as exprs but end up in semicolon
+# if the expression is inline, otherwise the semicolon is optional
+Stmt %= let + Binding + MoreBindings + in_k + Stmt, None, None, None, None, None, None
+Stmt %= (
+    if_k + lparen + Expr + rparen + Expr + ElseStmtBranch,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+)
+Stmt %= while_k + lparen + Expr + rparen + Stmt, None, None, None, None, None, None
+Stmt %= (
+    for_k + lparen + identifier + in_k + Expr + rparen + Stmt,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+)
+Stmt %= BlockExpr + OptionalSemicolon, None, None, None
+Stmt %= Disj + MoreDisjs + semicolon, None, None, None, None
+
+MoreStmts %= Stmt + MoreStmts, None, None, None
+MoreStmts %= GRAMMAR.Epsilon, None
+
+ElseStmtBranch %= (
+    elif_k + lparen + Expr + rparen + Expr + ElseStmtBranch,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+)
+ElseStmtBranch %= else_k + Stmt, None, None, None
 
 MoreDisjs %= disj + Disj + MoreDisjs, None, None, None, None
 MoreDisjs %= GRAMMAR.Epsilon, None
@@ -162,17 +185,16 @@ MoreConjs %= conj + Conj + MoreConjs, None, None, None, None
 MoreConjs %= GRAMMAR.Epsilon, None
 
 Conj %= not_t + Conj, None, None, None
-Conj %= Comparison, None, None
+Conj %= Arith + Comparison, None, None, None
 
-Comparison %= Arith + Comparer + Arith, None, None, None, None
-Comparison %= Arith + is_k + identifier, None, None, None, None
-
-Comparer %= lt, None, None
-Comparer %= gt, None, None
-Comparer %= le, None, None
-Comparer %= ge, None, None
-Comparer %= eq, None, None
-Comparer %= neq, None, None
+Comparison %= lt + Arith, None, None, None
+Comparison %= gt + Arith, None, None, None
+Comparison %= le + Arith, None, None, None
+Comparison %= ge + Arith, None, None, None
+Comparison %= eq + Arith, None, None, None
+Comparison %= neq + Arith, None, None, None
+Comparison %= is_k + identifier, None, None, None
+Comparison %= GRAMMAR.Epsilon, None
 
 Arith %= Term + MoreTerms, None, None, None
 
@@ -218,15 +240,28 @@ Atom %= lparen + Expr + rparen, None, None, None, None
 Mutation %= mut + Expr, None, None, None
 Mutation %= GRAMMAR.Epsilon, None
 
-Args %= Expr + MoreArgs, None, None, None
-Args %= GRAMMAR.Epsilon, None
-MoreArgs %= comma + Expr + MoreArgs, None, None, None, None
-MoreArgs %= GRAMMAR.Epsilon, None
 
 Action %= dot + identifier + Action, None, None, None, None
 Action %= lbracket + number + rbracket + Action, None, None, None, None, None
 Action %= lparen + Args + rparen + Action, None, None, None, None, None
 Action %= as_k + identifier + Action, None, None, None, None
 Action %= GRAMMAR.Epsilon, None
+
+Program %= TypeDeclList + Expr, None, None, None
+
+TypeDeclList %= TypeDecl + TypeDeclList, None, None, None
+TypeDeclList %= GRAMMAR.Epsilon, None
+
+TypeDecl %= type_k + lbrace + MemberDeclList + rbrace, None, None, None, None, None
+
+MemberDeclList %= MemberDecl + semicolon + MemberDeclList, None, None, None, None
+MemberDeclList %= GRAMMAR.Epsilon, None
+
+MemberDecl %= identifier + Member, None, None, None
+Member %= bind + TypeAnnotation + Expr, None, None, None, None
+Member %= lparen + Params + rparen + MethodBody, None, None, None, None, None
+
+MethodBody %= BlockExpr, None, None
+MethodBody %= then + Expr, None, None, None
 
 # endregion
