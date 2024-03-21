16ffee008b7c15242c59a25f90474194df6c35c3
jose_leiva <jseleivaf686@gmail.com>
2024-03-21 18:56:21 +0100

    add Protocol

--- bruce/tools/semantic/context.py
+++ bruce/tools/semantic/context.py
@@ -136,6 +136,24 @@ class Type:
 
     def __repr__(self):
         return str(self)
+    
+
+class Protocol:
+    def __init__(self, name:str) -> None:
+        self.name = name
+        self.parents:list[Protocol] = []
+        self.methods: list[Method] = []
+
+    def __eq__(self, __value: object) -> bool:
+        return self.name == __value.name
+
+
+    def extends(self, other) -> bool:
+        if other in self.parents:
+            return True
+        
+        return any(parent.extends(other) for parent in self.parents)
+        
 
 
 class ErrorType(Type):
