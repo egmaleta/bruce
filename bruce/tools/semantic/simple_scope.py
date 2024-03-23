e790d5a99c489fabe9a31afc640b85881fcf53da
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-03-23 15:36:41 -0400

    add simpler Scope impl (NOT FINISHED)

---
+++ bruce/tools/semantic/simple_scope.py
@@ -0,0 +1,38 @@
+class Scope:
+    def __init__(self, parent: "Scope" | None = None):
+        self.parent = parent
+        self.variables: dict[str, Variable] = {}
+        self.functions: dict[str, Function] = {}
+
+    def create_child(self):
+        return Scope(self)
+
+    def get_variable(self, name: str):
+        v = self.variables.get(name)
+        if v != None:
+            return v
+
+        if self.parent != None:
+            return self.parent.get_variable(name)
+
+    def has_variable(self, name: str):
+        return self.get_variable(name) != None
+
+    def create_variable(self, name: str, type: Type | None):
+        if self.has_variable(name):
+            raise SemanticError(f"variable '{name}' is already defined")
+
+        v = Variable(name, type)
+        self.variables[name] = v
+        return v
+
+    def mutate_variable(self, name: str, value: Any):
+        v = self.get_variable(name)
+        if v == None:
+            raise SemanticError(f"variable '{name}' hasn't been defined")
+
+        if v.is_mutable():
+            v.value = value
+            return True
+
+        return False
