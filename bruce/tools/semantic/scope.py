fc9a8f354df7837365db4201e80e3805fde8b049
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2024-03-23 16:04:12 -0400

    asd

--- bruce/tools/semantic/scope.py
+++ bruce/tools/semantic/scope.py
@@ -17,8 +17,7 @@ class FunctionInfo:
 
 class Scope:
     def __init__(self, parent=None):
-        self.local_vars = []
-        self.local_funcs = []
+        self.locals = []
         self.parent = parent
         self.children = []
         self.index = 0 if parent is None else len(parent)
@@ -33,33 +32,17 @@ class Scope:
 
     def define_variable(self, vname, vtype):
         info = VariableInfo(vname, vtype)
-        self.local_vars.append(info)
-        return info
-
-    def define_function(self, fname, params, type, body):
-        info = FunctionInfo(fname, params)
-        self.local_funcs.append(info)
+        self.locals.append(info)
         return info
 
     def find_variable(self, vname, index=None):
-        locals = self.local_vars if index is None else islice(self.locals, index)
+        locals = self.locals if index is None else itt.islice(self.locals, index)
         try:
             return next(x for x in locals if x.name == vname)
         except StopIteration:
             return (
                 self.parent.find_variable(vname, self.index)
-                if self.parent != None
-                else None
-            )
-
-    def find_function(self, fname, params, body, index=None):
-        local_funcs = self.local_funcs if index is None else islice(self.locals, index)
-        try:
-            return next(x for x in local_funcs if x.name)
-        except StopIteration:
-            return (
-                self.parent.find_function(fname, params, body, self.index)
-                if self.parent != None
+                if self.parent is None
                 else None
             )
 
