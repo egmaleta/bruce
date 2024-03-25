da6d4db88f5e0a4f60a550f72f335df534b6d2f8
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2024-03-25 11:28:44 -0400

    Fixing TypeChecker

--- bruce/visitor.py
+++ bruce/visitor.py
@@ -2,10 +2,7 @@ from .tools.semantic import SemanticError, Type, Proto
 from .tools.semantic.context import Context
 from .tools.semantic.scope import Scope
 from .tools import visitor
-from .ast import (ProgramNode, TypeNode, TypePropertyNode,
-                  FunctionNode, ProtocolNode, MethodSpecNode,
-                  BlockNode, LetExprNode, TypeInstancingNode, 
-                  BooleanNode, StringNode, NumberNode)
+from .ast import *
 from typing import Union
 
 
@@ -75,8 +72,8 @@ class TypeCollector(object):
 
 
 class TypeBuilder(object):
-    def __init__(self):
-        self.errors: list[str] = []
+    def __init__(self, errors: list[str] = []):
+        self.errors: list[str] = errors
 
         # type doesn't include None because current_type will be set before read
         self.current_type: Union[Type, Proto] = None
@@ -116,8 +113,6 @@ class TypeBuilder(object):
 
         for member in node.members:
             self.visit(member, ctx)
-
-        # TODO: handle parent args
 
     @visitor.when(TypePropertyNode)
     def visit(self, node: TypePropertyNode, ctx: Context):
@@ -179,49 +174,62 @@ class TypeChecker:
 
     @visitor.when(TypeNode)
     def visit(self, node: TypeNode, ctx: Context, scope: Scope):
-        self.current_type = ctx.get_type(node.type)
+        current_type: Type = get_safe_type(node.type, ctx)
+        scope.define_variable("self", current_type)
+        # This is to know if the args of the parents are ok
         if node.parent_type:
-            parent_type = ctx.get_type(node.parent_type)
-            parent_params_size = len(
+            parent_type = get_safe_type(node.parent_type, ctx)
+            parent_args_size = len(
                 parent_type.params) if parent_type.params else 0
             node_parent_args_size = len(
                 node.parent_args) if node.parent_args else 0
-            if parent_params_size != node_parent_args_size:
+            if parent_args_size != node_parent_args_size:
                 self.errors.append(
-                    f"Type {node.parent_type} expects {parent_params_size} arguments but {node_parent_args_size} were given"
+                    f"Type {node.parent_type} expects {parent_args_size} arguments but {node_parent_args_size} were given"
                 )
             if parent_type.params and node.parent_args:
                 for parent_arg, node_arg in zip(parent_type.params, node.parent_args):
-                    self.visit(node_arg, ctx, scope.create_child())
-                    if not self.current_type.conforms_to(parent_arg.type):
+                    arg_type = self.visit(node_arg, ctx, scope.create_child())
+                    if not arg_type.conforms_to(parent_arg.type):
                         self.errors.append(
-                            f"Cannot convert {self.current_type.name} into {parent_arg.type.name}"
+                            f"Cannot convert {arg_type.name} into {parent_arg.type.name}"
                         )
+        current_type.parent_args = node.parent_args
+
         for member in node.members:
             self.visit(member, ctx, scope.create_child())
 
     @visitor.when(FunctionNode)
-    def visit(self, node: FunctionNode, cxt: Context, scope: Scope):
+    def visit(self, node: FunctionNode, ctx: Context, scope: Scope):
         self.current_method = self.current_type.get_method(node.id)
         for param in node.params:
             self.visit(param, ctx, scope)
-        self.visit(node.body, cxt, scope.create_child())
+        self.visit(node.body, ctx, scope.create_child())
 
     @visitor.when(TypePropertyNode)
-    def visit(self, node: TypePropertyNode, scope: Scope):
-        self.visit(node.value, ctx, scope.create_child())
+    def visit(self, node: TypePropertyNode, ctx: Context, scope: Scope):
+        attributte_type = self.visit(node.value, ctx, scope.create_child())
+        scope.define_variable(node.id, attributte_type)
 
     @visitor.when(BlockNode)
-    def visit(self, node: BlockNode, scope: Scope):
+    def visit(self, node: BlockNode, ctx: Context, scope: Scope):
+        stack = []
         for member in node.exprs:
-            self.visit(member, ctx, scope.create_child())
+            stack.append(self.visit(member, ctx, scope.create_child()))
+        return_type = get_safe_type("Object", ctx)
+        while len(stack) > 0:
+            if stack[len(stack) - 1]:
+                return_type = stack.pop()
+                break
+            stack.pop()
+        return return_type
 
     @visitor.when(LetExprNode)
-    def visit(self, node: LetExprNode, cxt: Context, scope: Scope):
+    def visit(self, node: LetExprNode, ctx: Context, scope: Scope):
         # node.type = self.context.get_type("object").name if not node.type else node.type
         if scope.is_defined(node.id):
             self.errors.append(f"Variable {node.id} already defined")
-        self.visit(node.value, cxt, scope.create_child())
+        self.visit(node.value, ctx, scope.create_child())
         if node.type:
             node_type = self.context.get_type(node.type)
             if not self.current_type.conforms_to(node_type):
@@ -232,26 +240,31 @@ class TypeChecker:
         self.visit(node.body, ctx, scope.create_child())
 
     @visitor.when(TypeInstancingNode)
-    def visit(self, node: TypeInstancingNode, cxt: Context, scope: Scope):
-        self.current_type = self.context.get_type(node.type)
-        node_params_size = len(node.args) if node.args else 0
-        current_type_params_size = (
-            len(self.current_type.params) if self.current_type.params else 0
+    def visit(self, node: TypeInstancingNode, ctx: Context, scope: Scope):
+        # Check the size of args of the type and the instance
+        current_type = get_safe_type(node.type, ctx)
+        node_args_size = len(node.args) if node.args else 0
+        type_args_size = (
+            len(current_type.params) if current_type.params else 0
         )
-        if current_type_params_size != node_params_size:
+        if type_args_size != node_args_size:
             self.errors.append(
-                f"Type {node.type} expects {current_type_params_size} arguments, but {node_params_size} were given"
+                f"Type {node.type} expects {type_args_size} arguments, but {node_args_size} were given"
             )
 
         for arg in node.args:
-            self.visit(arg, ctx, scope.create_child())
+            arg_type = self.visit(arg, ctx, scope.create_child())
             node_type = self.context.get_type(node.type)
-            if not node_type.conforms_to(self.current_type):
+            if not node_type.conforms_to(arg_type):
                 self.errors.append(
-                    f"Cannot convert {self.current_type.name} to {node_type.name}"
+                    f"Cannot convert {arg_type.name} to {node_type.name}"
                 )
-        self.current_type = self.context.get_type(node.type)
+        return get_safe_type(node.type, ctx)
 
     @visitor.when(BooleanNode)
-    def visit(self, node: BooleanNode, cxt: Context, scope: Scope):
+    def visit(self, node: BooleanNode, ctx: Context, scope: Scope):
         self.current_type = self.context.get_type("Boolean")
+        
+    @visitor.when(IdentifierNode)
+    def visit(self, node: IdentifierNode, ctx: Context, scope: Scope):
+        return scope.find_variable(node.value).type
