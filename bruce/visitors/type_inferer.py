96b68b7a20d97b1a39e37d1ed029a19e3b574052
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-03-26 08:42:20 -0400

    infer literal types

--- bruce/visitors/type_inferer.py
+++ bruce/visitors/type_inferer.py
@@ -1,4 +1,8 @@
 from ..tools import visitor
+from ..tools.semantic.context import Context
+from ..tools.semantic.scope import Scope
+from .. import ast
+from .. import types
 
 
 class TypeInferer:
@@ -9,6 +13,18 @@ class TypeInferer:
     @visitor.on("node")
     def visit(self, node, ctx, scope):
         pass
+
+    @visitor.on(ast.NumberNode)
+    def visit(self, node, ctx, scope):
+        return types.NUMBER_TYPE
+
+    @visitor.on(ast.StringNode)
+    def visit(self, node, ctx, scope):
+        return types.STRING_TYPE
+
+    @visitor.on(ast.BooleanNode)
+    def visit(self, node, ctx, scope):
+        return types.BOOLEAN_TYPE
 
 
 # PD: I AM COOKING HERE...
