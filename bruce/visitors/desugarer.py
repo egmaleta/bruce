740305f8fb1b9c9c638b3a155719db74d6399400
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-03-27 12:01:56 -0400

    visit rest of expressions

--- bruce/visitors/desugarer.py
+++ bruce/visitors/desugarer.py
@@ -85,3 +85,123 @@ class Desugarer:
             )
 
         return node
+    
+    @visitor.when(ast.LiteralNode)
+    def visit(self, node: ast.LiteralNode):
+        return node
+    
+    @visitor.when(ast.TypeInstancingNode)
+    def visit(self, node: ast.TypeInstancingNode):
+        return ast.TypeInstancingNode(
+            node.type,
+            [self.visit(arg) for arg in node.args]
+        )
+
+    @visitor.when(ast.VectorNode)
+    def visit(self, node: ast.VectorNode):
+        return ast.VectorNode(
+            [self.visit(item) for item in node.items]
+        )
+
+    @visitor.when(ast.MappedIterableNode)
+    def visit(self, node: ast.MappedIterableNode):
+        return ast.MappedIterableNode(
+            self.visit(node.map_expr),
+            node.item_id,
+            node.item_type,
+            self.visit(node.iterable_expr)
+        )
+
+    @visitor.when(ast.MemberAccessingNode)
+    def visit(self, node: ast.MemberAccessingNode):
+        return ast.MemberAccessingNode(
+            self.visit(node.target),
+            node.member_id
+        )
+
+    @visitor.when(ast.FunctionCallNode)
+    def visit(self, node: ast.FunctionCallNode):
+        return ast.FunctionCallNode(
+            self.visit(node.target),
+            [self.visit(arg) for arg in node.args]
+        )
+
+    @visitor.when(ast.IndexingNode)
+    def visit(self, node: ast.IndexingNode):
+        return ast.IndexingNode(
+            self.visit(node.target),
+            self.visit(node.index)
+        )
+
+    @visitor.when(ast.MutationNode)
+    def visit(self, node: ast.MutationNode):
+        return ast.MutationNode(
+            self.visit(node.target),
+            self.visit(node.value)
+        )
+
+    @visitor.when(ast.DowncastingNode)
+    def visit(self, node: ast.DowncastingNode):
+        return ast.DowncastingNode(
+            self.visit(node.target),
+            node.type
+        )
+
+    @visitor.when(ast.UnaryOpNode)
+    def visit(self, node: ast.UnaryOpNode):
+        return type(node)(self.visit(node.operand))
+
+    @visitor.when(ast.BinaryOpNode)
+    def visit(self, node: ast.BinaryOpNode):
+        left = self.visit(node.left)
+        right = self.visit(node.right)
+
+        if isinstance(node, (ast.PowerOpNode, ast.ConcatOpNode)):
+            return type(node)(left, right)
+        
+        return type(node)(left, node.operator, right)
+    
+
+    @visitor.when(ast.TypeMatchingNode)
+    def visit(self, node: ast.TypeMatchingNode):
+        return ast.TypeMatchingNode(
+            self.visit(node.target),
+            node.type
+        )
+
+    @visitor.when(ast.BlockNode)
+    def visit(self, node: ast.BlockNode):
+        return ast.BlockNode(
+            [self.visit(expr) for expr in node.exprs]
+        )
+
+    @visitor.when(ast.LoopNode)
+    def visit(self, node: ast.LoopNode):
+        return ast.LoopNode(
+            self.visit(node.condition),
+            self.visit(node.body),
+            self.visit(node.fallback_expr)
+        )
+
+    @visitor.when(ast.ConditionalNode)
+    def visit(self, node: ast.ConditionalNode):
+        return ast.ConditionalNode(
+            [(self.visit(c), self.visit(b)) for c, b in node.condition_branchs],
+            self.visit(node.fallback_branck)
+        )
+
+    @visitor.when(ast.FunctionNode)
+    def visit(self, node: ast.FunctionNode):
+        pass
+
+    @visitor.when(ast.ProtocolNode)
+    def visit(self, node: ast.ProtocolNode):
+        pass
+
+    @visitor.when(ast.TypeNode)
+    def visit(self, node: ast.TypeNode):
+        pass
+
+    @visitor.when(ast.ProgramNode)
+    def visit(self, node: ast.ProgramNode):
+        pass
