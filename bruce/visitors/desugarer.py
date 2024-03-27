fd4dab942ab1f4c8f7ee882e0d3bb04ba6491810
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-03-27 15:47:49 -0400

    visit program node and format

--- bruce/visitors/desugarer.py
+++ bruce/visitors/desugarer.py
@@ -85,23 +85,18 @@ class Desugarer:
             )
 
         return node
-    
+
     @visitor.when(ast.LiteralNode)
     def visit(self, node: ast.LiteralNode):
         return node
-    
+
     @visitor.when(ast.TypeInstancingNode)
     def visit(self, node: ast.TypeInstancingNode):
-        return ast.TypeInstancingNode(
-            node.type,
-            [self.visit(arg) for arg in node.args]
-        )
+        return ast.TypeInstancingNode(node.type, [self.visit(arg) for arg in node.args])
 
     @visitor.when(ast.VectorNode)
     def visit(self, node: ast.VectorNode):
-        return ast.VectorNode(
-            [self.visit(item) for item in node.items]
-        )
+        return ast.VectorNode([self.visit(item) for item in node.items])
 
     @visitor.when(ast.MappedIterableNode)
     def visit(self, node: ast.MappedIterableNode):
@@ -109,43 +104,30 @@ class Desugarer:
             self.visit(node.map_expr),
             node.item_id,
             node.item_type,
-            self.visit(node.iterable_expr)
+            self.visit(node.iterable_expr),
         )
 
     @visitor.when(ast.MemberAccessingNode)
     def visit(self, node: ast.MemberAccessingNode):
-        return ast.MemberAccessingNode(
-            self.visit(node.target),
-            node.member_id
-        )
+        return ast.MemberAccessingNode(self.visit(node.target), node.member_id)
 
     @visitor.when(ast.FunctionCallNode)
     def visit(self, node: ast.FunctionCallNode):
         return ast.FunctionCallNode(
-            self.visit(node.target),
-            [self.visit(arg) for arg in node.args]
+            self.visit(node.target), [self.visit(arg) for arg in node.args]
         )
 
     @visitor.when(ast.IndexingNode)
     def visit(self, node: ast.IndexingNode):
-        return ast.IndexingNode(
-            self.visit(node.target),
-            self.visit(node.index)
-        )
+        return ast.IndexingNode(self.visit(node.target), self.visit(node.index))
 
     @visitor.when(ast.MutationNode)
     def visit(self, node: ast.MutationNode):
-        return ast.MutationNode(
-            self.visit(node.target),
-            self.visit(node.value)
-        )
+        return ast.MutationNode(self.visit(node.target), self.visit(node.value))
 
     @visitor.when(ast.DowncastingNode)
     def visit(self, node: ast.DowncastingNode):
-        return ast.DowncastingNode(
-            self.visit(node.target),
-            node.type
-        )
+        return ast.DowncastingNode(self.visit(node.target), node.type)
 
     @visitor.when(ast.UnaryOpNode)
     def visit(self, node: ast.UnaryOpNode):
@@ -158,50 +140,59 @@ class Desugarer:
 
         if isinstance(node, (ast.PowerOpNode, ast.ConcatOpNode)):
             return type(node)(left, right)
-        
+
         return type(node)(left, node.operator, right)
-    
 
     @visitor.when(ast.TypeMatchingNode)
     def visit(self, node: ast.TypeMatchingNode):
-        return ast.TypeMatchingNode(
-            self.visit(node.target),
-            node.type
-        )
+        return ast.TypeMatchingNode(self.visit(node.target), node.type)
 
     @visitor.when(ast.BlockNode)
     def visit(self, node: ast.BlockNode):
-        return ast.BlockNode(
-            [self.visit(expr) for expr in node.exprs]
-        )
+        return ast.BlockNode([self.visit(expr) for expr in node.exprs])
 
     @visitor.when(ast.LoopNode)
     def visit(self, node: ast.LoopNode):
         return ast.LoopNode(
             self.visit(node.condition),
             self.visit(node.body),
-            self.visit(node.fallback_expr)
+            self.visit(node.fallback_expr),
         )
 
     @visitor.when(ast.ConditionalNode)
     def visit(self, node: ast.ConditionalNode):
         return ast.ConditionalNode(
             [(self.visit(c), self.visit(b)) for c, b in node.condition_branchs],
-            self.visit(node.fallback_branck)
+            self.visit(node.fallback_branck),
         )
-
-    @visitor.when(ast.FunctionNode)
-    def visit(self, node: ast.FunctionNode):
-        pass
-
-    @visitor.when(ast.ProtocolNode)
-    def visit(self, node: ast.ProtocolNode):
-        pass
-
-    @visitor.when(ast.TypeNode)
-    def visit(self, node: ast.TypeNode):
-        pass
 
     @visitor.when(ast.ProgramNode)
     def visit(self, node: ast.ProgramNode):
-        pass
+        function_nodes: list[ast.FunctionNode] = []
+        type_nodes: list[ast.TypeNode] = []
+
+        for decl in node.declarations:
+            if isinstance(decl, ast.FunctionNode):
+                function_nodes.append(decl)
+            elif isinstance(decl, ast.TypeNode):
+                type_nodes.append(decl)
+
+        for fn in function_nodes:
+            fn.body = self.visit(fn.body)
+
+        for tn in type_nodes:
+            self.current_type_parent_name = tn.parent_type
+
+            if tn.parent_args is not None:
+                tn.parent_args = [self.visit(arg) for arg in tn.parent_args]
+
+            for member in tn.members:
+                if isinstance(member, ast.TypePropertyNode):
+                    member.value = self.visit(member.value)
+                elif isinstance(member, ast.FunctionNode):
+                    self.current_method_name = member.id
+                    member.body = self.visit(member.body)
+
+        node.expr = self.visit(node.expr)
+
+        return node
