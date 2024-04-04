00ad82e266e26b6dd94d0879bf764491bb4c4949
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-04-04 16:32:51 -0400

    format

--- bruce/tools/parser.py
+++ bruce/tools/parser.py
@@ -268,7 +268,9 @@ def create_parser(
 
             if not stack:
                 if cursor < len(tokens) - 1:
-                    raise UnexpectedToken(current_token.lex, None, current_token.position[0])
+                    raise UnexpectedToken(
+                        current_token.lex, None, current_token.position[0]
+                    )
                 break
 
         return output
