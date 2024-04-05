3f5dd56e19b1ef981f6569deee43ef35b1294db0
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2024-04-04 23:10:49 -0400

    Fix conform_to with uniontypes

--- bruce/types.py
+++ bruce/types.py
@@ -134,7 +134,17 @@ class UnionType(Type):
         return False
 
     def conforms_to(self, other: "Type"):
-        return any(t.conforms_to(other) for t in self.types)
+        if not isinstance(other, UnionType):
+            return any(t.conforms_to(other) for t in self.types)
+        
+        if len(self.types) != len(other.types):
+            return False
+        
+        for t1, t2 in zip(self.types, other.types):
+            if t1 != t2:
+                return False
+            
+        return True
 
     def __eq__(self, other):
         return isinstance(other, UnionType) and self.types == other.types
